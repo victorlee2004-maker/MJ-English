bd28c0fe403956fe3e3252714aea0f5c6c413eb4
victorlee2004-maker <victorlee2004@gmail.com>
2026-06-02 15:12:03 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'english-app-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './train.html',
+  './progress.html',
+  './tips.html',
+  './app.js',
+  './data.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
